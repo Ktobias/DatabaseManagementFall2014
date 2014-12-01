b58f8f031d000f05454c003d19ce4e0d7e964e4f
Kimberly T <ktobias@ca.home>
2014-12-01 01:10:12 -0500

    Assignment10 --Stored Procedures

---
+++ Assignment10.sql
@@ -0,0 +1,43 @@
+﻿--Kimberly Tobias
+--Assignment 10
+--Due December 1, 2014
+
+--1. function PreReqsFor(courseNum)- Returns the immediate prerequisites for the passed-in course number
+Drop function PreReqsFor(integer);
+CREATE OR REPLACE FUNCTION PreReqsFor(courseInt integer)
+RETURNS SETOF Courses as $$
+BEGIN
+  RETURN query 
+  SELECT * 
+  FROM Courses
+  WHERE num IN (
+	SELECT preReqNum 
+	FROM prerequisites
+	WHERE courseInt = courseNum
+  );
+END;
+$$ LANGUAGE 'plpgsql';
+
+
+select PreReqsFor(499);
+
+
+
+--2. function IsPreReqsFor(courseNum)- Returns the the courses for which the passed in course number is an immediate pre-req
+Drop function IsPreReqsFor(integer);
+CREATE OR REPLACE FUNCTION IsPreReqsFor(courseInt integer)
+RETURNS SETOF Courses as $$
+BEGIN
+  RETURN query 
+  SELECT * 
+  FROM Courses
+  WHERE num IN (
+	SELECT courseNum 
+	FROM prerequisites
+	WHERE courseInt = preReqNum
+  );
+END;
+$$ LANGUAGE 'plpgsql';
+
+
+select isprereqsfor(308);
